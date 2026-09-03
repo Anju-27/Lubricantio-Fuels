574b74dcdb236d2f0fd786bd1be8dab72bae30ee
Anju-27 <anjalipatel2762003@gmail.com>
2026-09-03 12:22:23 +0530

    Add Lubricantio Fuels website files

---
+++ inc/footer.php
@@ -0,0 +1,138 @@
+	  <!-- Call Action -->
+	  <div class="call-action cta-mini bg-secondary">
+	    <div class="cta-block">
+	      <div class="container">
+	        <div class="content row">
+
+	          <div class="cta-sameline">
+	            <h3>Reach out to us anytime and we'll happily answer your questions!</h3>
+	            <span class="cta-num">Call <strong><a href="tel:9711951251">+91- 9711951251</a></strong></span>
+	          </div>
+
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	  <!-- End Section -->
+
+
+	  <!-- Footer Widget-->
+	  <div class="footer-widget section-pad">
+	    <div class="container">
+	      <div class="row">
+
+	        <div class="widget-row row">
+	          <div class="footer-col col-md-5 col-sm-6 res-m-bttm">
+	            <!-- Each Widget -->
+	            <div class="wgs wgs-footer wgs-menu">
+	              <h5 class="wgs-title">About Us</h5>
+	              <div class="wgs-content">
+	                <p>Lubricantio Fuels Provides the Advance Product for Automotive, Industrial And Specialty
+	                  Applications.
+	                  Around The World Lubricantio Fuels Are Recognized for A Legacy of Technological Innovation
+	                  Performance and Reliability.
+	                  Our Company Believes In The Core Value Of Integrity, Honesty And Respect For The People Of Our
+	                  Country And Abroad.</p>
+	              </div>
+	            </div>
+	            <!-- End Widget -->
+	          </div>
+	          <div class="footer-col col-md-2 col-sm-6 res-m-bttm">
+	            <!-- Each Widget -->
+	            <div class="wgs wgs-footer wgs-menu">
+	              <h5 class="wgs-title">Quick Links</h5>
+	              <div class="wgs-content">
+	                <ul class="menu">
+	                  <li><a href="index.php">Home</a></li>
+
+	                  <li><a href="about.php">About Us</a></li>
+	                  <li>
+	                    <a href="service.php">Services</a>
+
+	                  </li>
+	                  <li><a href="product.php">Products</a></li>
+
+	                  <li><a href="contact.php">Contact Us</a></li>
+	                </ul>
+	              </div>
+	            </div>
+	            <!-- End Widget -->
+	          </div>
+	          <div class="footer-col col-md-2 col-sm-6 res-m-bttm">
+	            <!-- Each Widget -->
+	            <div class="wgs wgs-footer wgs-text">
+	              <h5 class="wgs-title">Services</h5>
+	              <div class="wgs-content">
+	                <ul>
+	                  <li><a href="service.php">Private Labelling</a></li>
+	                  <li><a href="service.php">Toll Blending</a></li>
+	                  <li><a href="service.php">Industries Served</a></li>
+	                  <li><a href="service.php">Infrastructure</a></li>
+	                </ul>
+	              </div>
+	            </div>
+	            <!-- End Widget -->
+	          </div>
+	          <div class="footer-col col-md-3 col-sm-6">
+	            <!-- Each Widget -->
+	            <div class="wgs wgs-footer"> 
+	              <h5 class="wgs-title">Contact us</h5>
+	              <div class="wgs-content">
+	                <p>
+									PASCHIM VIHAR NEW DELHI 110063
+	                </p>
+	                <p>
+	                  <span>Phone</span>:  9711951251
+	                </p>
+	                <p style="text-transform:lowercase;">
+	                  <span style="
+    text-transform: capitalize;
+">Email</span>: Info@Lubricantiofuels.Co.In
+	                </p>
+	                <ul class="social">
+	                  <li><a href="#"><em class="fa fa-facebook" aria-hidden="true"></em></a></li>
+	                  <li><a href="#"><em class="fa fa-twitter" aria-hidden="true"></em></a></li>
+	                  <li><a href="#"><em class="fa fa-linkedin" aria-hidden="true"></em></a></li>
+	                </ul>
+	              </div>
+	            </div>
+	            <!-- End Widget -->
+	          </div>
+
+	        </div>
+
+	      </div>
+	    </div>
+	  </div>
+	  <!-- End Footer Widget -->
+
+	  <!-- Copyright -->
+	  <div class="copyright text-center">
+	    <div class="container">
+	      <div class="row">
+	        <div class="site-copy col-sm-12">
+	          <p style="color:#fff;">© 2024 Lubricantio Fuels . designed by <a href="https://webcadenceindia.com/" target="_blank"> Webcadenceindia.com</a></p>
+	        </div>
+
+	      </div>
+	    </div>
+	  </div>
+	  <!-- End Copyright -->
+
+	  <!-- Rreload Image for Slider -->
+	  <div class="preload hide">
+	    <img alt="" src="image/slider-constuct-b.jpg">
+	    <img alt="" src="image/slider-constuct-a.jpg">
+	  </div>
+	  <!-- End -->
+
+	  <!-- Preloader !active please if you want -->
+	  <!-- <div id="preloader"><div id="status">&nbsp;</div></div> -->
+	  <!-- Preloader End -->
+
+	  <!-- JavaScript Bundle -->
+	  <script src="js/jquery.bundle.js"></script>
+	  <!-- Theme Script init() -->
+	  <script src="js/script.js"></script>
+	  <!-- End script -->
+
---
+++ inc/header.php
@@ -0,0 +1,206 @@
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="icon" href="image/favicon.png" type="image/png" sizes="16x16">
+	<link rel="stylesheet" type="text/css" href="css/vendor.bundle.css">
+	<link id="style-css" rel="stylesheet" type="text/css" href="css/style62ea.css?ver=1.2">
+	<style type="text/css">
+.colorPanel {
+  margin: 0px;
+  padding: 5px;
+  position: fixed;
+  z-index: 100;
+  min-width: 20px;
+  border-radius: 4px 0 0 4px;
+  background-color: #4b00a0;
+  right: 0;
+  top: 33%;
+}
+
+.colorPanel ul {
+  margin: 0px;
+  padding: 0px;
+  list-style: none;
+  display: none;
+}
+
+.colorPanel ul li {
+  display: block;
+  margin-top: 10px;
+}
+
+.colorPanel ul a {
+  display: block;
+  width: 20px;
+  height: 20px;
+  border: #fff 1px solid;
+}
+
+.colorPanel a.cart {
+  border-bottom: 1px solid rgba(255, 255, 255, .3);
+  margin-bottom: 6px;
+  padding-bottom: 8px;
+  display: block;
+}
+
+#cpToggle {
+  display: block;
+  height: 30px;
+  width: 20px;
+  line-height: 30px;
+  background-size: cover;
+}
+
+.cp-custom {
+  padding: 12px;
+}
+
+.cp-custom #cpToggle {
+  background: none;
+}
+
+.cp-custom i {
+  font-size: 15px;
+  color: #fff;
+}
+	</style>
+	<script>
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '../../www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-91615293-2', 'auto');
+ga('send', 'pageview');
+	</script>
+	</head>
+	<!---whtsaap--->
+	<script>
+var url = 'https://wati-integration-prod-service.clare.ai/v2/watiWidget.js?48520';
+var s = document.createElement('script');
+s.type = 'text/javascript';
+s.async = true;
+s.src = url;
+var options = {
+  "enabled": true,
+  "chatButtonSetting": {
+    "backgroundColor": "#05bd11",
+    "ctaText": "",
+    "borderRadius": "50",
+    "marginLeft": "50",
+    "marginRight": "0",
+    "marginBottom": "50",
+    "ctaIconWATI": false,
+    "position": "left"
+  },
+  "brandSetting": {
+    "brandName": "lubricantiofuel",
+    "brandSubTitle": "undefined",
+    "brandImg": "https://www.wati.io/wp-content/uploads/2023/04/Wati-logo.svg",
+    "welcomeText": "Hi there!\nHow can I help you?",
+    "messageText": "",
+    "backgroundColor": "#05bd11",
+    "ctaText": "",
+    "borderRadius": "50",
+    "autoShow": false,
+    "phoneNumber": "919891143537"
+  }
+};
+s.onload = function() {
+  CreateWhatsappChatWidget(options);
+};
+var x = document.getElementsByTagName('script')[0];
+x.parentNode.insertBefore(s, x);
+	</script>
+
+	<body class="site-body style-v1">
+	  <!-- Header -->
+	  <header class="site-header header-s1 is-sticky">
+	    <!-- Topbar -->
+	    <div class="topbar style-dark">
+	      <div class="container">
+	        <div class="row">
+	          <div class="top-aside top-left">
+	            <ul class="social">
+	              <li><a href="#"><em class="fa fa-facebook" aria-hidden="true"></em></a></li>
+	              <li><a href="#"><em class="fa fa-twitter" aria-hidden="true"></em></a></li>
+	              <li><a href="#"><em class="fa fa-linkedin" aria-hidden="true"></em></a></li>
+	            </ul>
+	            
+	          </div>
+
+
+	          <div class="top-aside top-right clearfix">
+				
+	            <ul class="top-contact clearfix">
+							<li class="t-email t-email1">
+								<span class="text-center ht-h">An ISO 9001:2015</span>
+	              </li>
+	              <li class="t-email t-email1">
+	                <em class="fa fa-envelope-o" aria-hidden="true"></em>
+	                <span><a href="mailto:Info@Lubricantiofuels.Co.In">Info@Lubricantiofuels.Co.In</a></span>
+	              </li>
+	              <li class="t-phone t-phone1">
+	                <em class="fa fa-phone" aria-hidden="true"></em>
+	                <span>+91-9711951251</span>
+	              </li>
+	            </ul>
+	          </div>
+	        </div>
+	      </div>
+	    </div>
+	    <!-- #end Topbar -->
+	    <!-- Navbar -->
+	    <div class="navbar navbar-primary">
+	      <div class="container">
+	        <!-- Logo -->
+	        <a class="navbar-brand" href="index.php">
+	          <img class="logo logo-dark" alt="" src="image/logo.png">
+	          <img class="logo logo-light" alt="" src="image/logo-light.png">
+	        </a>
+	        <!-- #end Logo -->
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mainnav"
+	            aria-expanded="false">
+	            <span class="sr-only">Menu</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <!-- Q-Button for Mobile -->
+	          <div class="quote-btn"><a a class="btn" href="tel: 9711951251">Call Now</a></div>
+	        </div>
+	        <!-- MainNav -->
+	        <nav class="navbar-collapse collapse" id="mainnav">
+	          <ul class="nav navbar-nav">
+	            <li><a href="index.php">Home</a></li>
+
+	            <li><a href="about.php">About Us</a></li>
+	            <li class="dropdown">
+	              <a href="service.php">Services</a>
+	              <ul class="dropdown-menu">
+	                <li><a href="service.php">Private Labelling</a></li>
+	                <li><a href="service.php">Toll Blending</a></li>
+	                <li><a href="service.php">Industries Served</a></li>
+	                <li><a href="service.php">Infrastructure</a></li>
+	              </ul>
+	            </li>
+	            <li><a href="product.php">Products</a></li>
+							<li><a href="gallery.php">Gallery</a></li>
+	            <li><a href="contact.php">Contact Us</a></li>
+	            <li class="quote-btn"><a class="btn" href="tel: 9711951251">Call Now</a></li>
+	          </ul>
+	        </nav>
+	        <!-- #end MainNav -->
+	      </div>
+	    </div>
+	    <!-- #end Navbar -->
+
+	  </header>
+	  <!-- End Header -->
